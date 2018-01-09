340ac5e12d794a72fbdae4f3c82be1be95defb72
Parth Pahwa <parth.pahwa95@gmail.com>
2018-01-09 15:51:44 +0530

    Policy network based agent added

--- Cart Pole/cart_pole_sol.py
+++ Cart Pole/cart_pole_sol.py
@@ -1,12 +1,17 @@
 import gym
 import tensorflow as tf
 import numpy as np
+from cart_pole_agent import Agent
 
 env = gym.make('CartPole-v0')
 n_actions = env.action_space.n
-# n_states = env.observation_space.n
+
+gamma = 0.95
 num_episodes = 2000
 max_eps = 500
+update_freq = 5
+agent = Agent(2, 4, 15)
+
 
 def discounted_reward(r):
 	d_reward = np.zeros_like(r)
@@ -16,17 +21,22 @@ def discounted_reward(r):
 		d_reward[t] = r_reward
 	return d_reward
 
+
 with tf.Session() as sess:
 	i = 0
 	total_reward = []
-	total_lenght = []
+	gradBuffer = agent.trainable_vars
+
+	for ix, grad in enumerate(gradBuffer):
+		gradBuffer[ix] = grad * 0
+
 	while i < num_episodes:
 		running_reward = 0
 		current_state = env.reset()
 		j = 0
 		ep_history = []
 		while j < max_eps:
-			action = np.random.randint(0, n_actions)
+			action = agent.predict(current_state)
 			next_state, reward, done, _ = env.step(action)
 			running_reward += reward
 			ep_history.append([current_state, action, reward, next_state])
@@ -35,10 +45,19 @@ with tf.Session() as sess:
 			if done:
 				ep_history = np.array(ep_history)
 				ep_history[:, 2] = discounted_reward(ep_history[:, 2])
+				grads = np.array(agent.compute_gradient(ep_history))
+
+				for ix, grad in enumerate(grads):
+					gradBuffer[ix] += grad
+
+				if i % update_freq == 0 and i != 0:
+					feed_dict = dictionary = dict(zip(agent.grad_list, gradBuffer))
+					agent.learn(feed_dict)
+					for ix, grad in enumerate(grads):
+						gradBuffer[ix] = grad * 0
+				total_reward.append(running_reward)
 				break
 			j += 1
-
-		print "Reward: " + str(running_reward) + "Steps: " + str(j)
-		total_reward.append(running_reward)
-		total_lenght.append(j)
+		if i % 100 == 0:
+			print np.mean(running_reward[-100:])
 		i += 1
