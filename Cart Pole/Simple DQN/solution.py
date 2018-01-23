aed6038353ce7049819d3be9d0ff5bef2832a617
Parth Pahwa <parth.pahwa95@gmail.com>
2018-01-23 21:20:17 +0530

    Functionality to save and load DNN weights added

--- Cart Pole/Simple DQN/solution.py
+++ Cart Pole/Simple DQN/solution.py
@@ -1,21 +1,23 @@
+import os.path
 import gym
 from agent import Agent
+import numpy as np
+from keras.models import load_model
+
 
 class Environment:
 	def __init__(self, environment_name):
 		self.env = gym.make(environment_name)
+		self.q_val = []
 
 	def run(self, agent):
 		state = self.env.reset()
 		total_reward = 0
-
 		while True:
-			self.env.render()
-
-			action = agent.predict_single(state)
+			action, self.q_val = agent.predict_single(state)
 			next_state, reward, done, _ = self.env.step(action)
 
-			if done:
+			if done: # terminal state
 				next_state = [None for itr in range(0, self.env.observation_space.shape[0])]
 
 			agent.save(state, next_state, action, reward)
@@ -27,13 +29,25 @@ class Environment:
 			if done:
 				break
 
-		print("Total reward:", total_reward)
+		return total_reward, self.q_val
 
 
 cart_pole = Environment('CartPole-v0')
-print cart_pole.env.observation_space.shape[0], cart_pole.env.action_space.n
 agent = Agent(cart_pole.env.observation_space.shape[0], cart_pole.env.action_space.n, 64)
 
-while True:
-	cart_pole.run(agent)
-	agent.decay()
+itr = 0
+reward_list = []
+try:
+	if os.path.isfile("weights.h5"):
+		agent.model = load_model("weights.h5")
+
+	while True:
+		reward, q_val = cart_pole.run(agent)
+		print "Reward:", reward, "Q value:", q_val, "Iteration:", itr
+		agent.decay()
+		reward_list.append(reward)
+		if itr % 100 == 0 and itr != 0:
+			print np.mean(reward_list[-100:])
+		itr += 1
+finally:
+	agent.model.save("wights.h5")
