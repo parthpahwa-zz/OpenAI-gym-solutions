77ade7438317aad43a96ad8de284938b0a8d6cdb
Parth Pahwa <parth.pahwa95@gmail.com>
2018-03-06 21:30:41 +0530

    Update memory.py

--- DeepQNetwork/memory.py
+++ DeepQNetwork/memory.py
@@ -5,18 +5,63 @@ import numpy as np
 class Memory:
 
 	def __init__(self, capacity):
+		self.load = False
 		self.capacity = capacity
 		self.transition_list = []
+		self.under_sampled_rewards = []
+		self.under_sampled_zeros = []
+		self.length = [1.0, 1.0]
 		self.savefile_name = 'memory.npy'
 
 
-	def add(self, action_replay):
+	def add(self, action_replay, perform_under_sampling=False):
+		if perform_under_sampling:
+			if action_replay[len(action_replay) - 1] and self.length[0]/self.length[1] <= 1.2:
+				self.under_sampled_rewards.append(action_replay)
+				self.length[0] += 1
+			elif action_replay[len(action_replay) - 1] == 0 and self.length[1]/self.length[0] <= 1.2:
+				self.under_sampled_zeros.append(action_replay)
+				self.length[1] += 1
+			
+			if sum(self.length) > self.capacity + 2:
+				self.under_sampled_rewards.pop()
+				self.length[0] -= 1
+
+				self.under_sampled_zeros.pop()
+				self.length[1] -= 1
+			return
+
 		self.transition_list.append(action_replay)
 		if len(self.transition_list) > self.capacity:
 			self.transition_list.pop()
 
 
-	def sample(self, n):
+	def sample(self, n, perform_under_sampling=False):
+		if perform_under_sampling:
+			if self.load:
+				print "LOADED", len(self.transition_list)
+				self.load = False
+				for val in self.transition_list:
+					if val[len(val)-1]:
+						self.under_sampled_rewards.append(val)
+						self.length[0] += 1
+					else:
+						self.under_sampled_zeros.append(val)
+						self.length[1] += 1
+			print self.length
+			self.transition_list = []
+			if n + 2 > sum(self.length):
+				tmpList = []
+				tmpList.extend(self.under_sampled_rewards)
+				tmpList.extend(self.under_sampled_zeros)
+				random.shuffle(tmpList)
+				return np.array(tmpList)
+
+			self.transition_list.extend(random.sample(self.under_sampled_rewards, min(n/2, int(self.length[0])-1)))
+			self.transition_list.extend(random.sample(self.under_sampled_zeros, min(n/2, int(self.length[1])-1)))
+			random.shuffle(self.transition_list)
+			return np.array(self.transition_list)
+
 		if n > len(self.transition_list):
 			return np.array(self.transition_list)
 		return np.array(random.sample(self.transition_list, n))
@@ -31,4 +76,5 @@ class Memory:
 	def load_memory(self):
 		if os.path.isfile(self.savefile_name):
 			self.transition_list = list(np.load(self.savefile_name))
-			print "Memory Loaded"
+			print "Memory Loaded", len(self.transition_list)
+			self.load = True
