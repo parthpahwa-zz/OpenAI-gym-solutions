3f7769483783568f0653799fba769100c6749840
Parth Pahwa <parth.pahwa95@gmail.com>
2018-03-06 21:31:44 +0530

    Update solution.py

--- DeepQNetwork/solution.py
+++ DeepQNetwork/solution.py
@@ -12,19 +12,19 @@ class Player:
 	def __init__(self):
 		self.game = Environment('SpaceInvaders-v0', WIDTH, HEIGHT, N_FRAMES)
 		self.agent = Agent(HEIGHT, WIDTH, self.game.env.action_space.n, nframes=N_FRAMES)
-		self.weights_filename = "../Records/weights.h5"
-		self.config_filename = '../Records/config.txt'
-		self.score_filename = '../Records/score.txt'
-		self.qval_filename = '../Records/q_val.txt'
-		if not os.path.exists("../Records"):
-			os.makedirs("../Records")
+		self.weights_filename = "Records/weights.h5"
+		self.config_filename = 'Records/config.txt'
+		self.score_filename = 'Records/score.txt'
+		self.qval_filename = 'Records/q_val.txt'
+		if not os.path.exists("Records"):
+			os.makedirs("Records")
 
-	def train(self, early_evaluate=False):
+	def train(self, early_evaluate=False, undersampled=False):
 		itr, avg, max_score = self.load_config()
 
 		try:
-			while self.agent.counter < 7000000:
-				reward, q_val_list, q_val = self.game.train(self.agent)
+			while self.agent.counter < 50000000:
+				reward, q_val_list, q_val = self.game.train(self.agent, undersampled)
 
 				if reward > max_score:
 					max_score = reward
@@ -55,15 +55,16 @@ class Player:
 			itr += 1
 
 		if avg <= total_reward/20.0:
-			if os.path.isfile("../Records/best_performace_" + str(avg) + ".h5"):
-				os.remove("../Records/best_performace_" + str(avg) + ".h5")
+			if os.path.isfile("Records/best_performace_" + str(avg) + ".h5"):
+				os.remove("Records/best_performace_" + str(avg) + ".h5")
 			avg = total_reward/20.0
-			self.agent.model.save("../Records/best_performace_" + str(avg) + ".h5")
+			self.agent.model.save("Records/best_performace_" + str(avg) + ".h5")
 		return avg
 
 
 	def test(self):
-		self.load_config(test=True)
+		self.load_config()
+		print "LOADED"
 		itr = 0
 		try:
 			while itr < 100:
@@ -102,13 +103,13 @@ class Player:
 			itr = int(config["itr"])
 			avg = float(config["avg"])
 			max_score = float(config["max"])
-			self.agent.counter = float(config["avg"])
-			self.agent.eps = int(config["frame"])
+			self.agent.counter = float(config["frame"])
+			self.agent.eps = float(config["eps"])
 			config_file.close()
 			print "Config loaded"
 
 			if test:
-				self.agent.model = load_model("../Records/best_performace_" + str(avg) + ".h5")
+				self.agent.model = load_model("Records/best_performace_" + str(avg) + ".h5")
 				print "Weights loaded"
 			return itr, avg, max_score
 
