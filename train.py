ad7d678db4d3da83183aaa892c1c507a74acac41
Parth Pahwa <parth.pahwa95@gmail.com>
2018-03-09 09:39:44 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from DeepQNetwork import solution
 
 PERFORM_UNDER_SAMPLING = True
+ENV_NAME = "SpaceInvaders-v0"
 
-player = solution.Player()
-player.train(undersampled=PERFORM_UNDER_SAMPLING)
+player = solution.Player(ENV_NAME)
+player.train(early_evaluate=True, undersampled=PERFORM_UNDER_SAMPLING)
