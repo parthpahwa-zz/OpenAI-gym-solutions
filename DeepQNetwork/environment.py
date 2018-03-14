a40d75e39aef4ddfd413f48ac307fc67b33155b2
Parth Pahwa <parth.pahwa95@gmail.com>
2018-03-14 08:59:13 +0530

    Update environment.py

--- DeepQNetwork/environment.py
+++ DeepQNetwork/environment.py
@@ -1,6 +1,6 @@
 import gym
 import numpy as np
-import cv2
+from .utils import preprocess
 
 class Environment:
 	def __init__(self, environment_name, WIDTH, HEIGHT, N_FRAMES):
@@ -13,8 +13,8 @@ class Environment:
 		q_val_list = []
 		state = self.env.reset()
 		total_reward = 0
-		state = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
-		state = cv2.resize(state, (self.HEIGHT, self.WIDTH))
+
+		state = preprocess(state, self.HEIGHT, self.WIDTH)
 		current_frame_buffer = [state, state, state, state]
 		next_frame_buffer = [state, state, state, state]
 
@@ -26,12 +26,11 @@ class Environment:
 			if done: # terminal state
 				next_state = None
 			else:
-				next_state = cv2.cvtColor(next_state, cv2.COLOR_BGR2GRAY)
-				next_state = cv2.resize(next_state, (self.HEIGHT, self.WIDTH))
+				next_state = preprocess(next_state, self.HEIGHT, self.WIDTH)
 
 			next_frame_buffer.pop()
 			next_frame_buffer.append(next_state)
-			
+
 			q_val_list.append(np.mean(q_val))
 			if reward > 0:
 				clipped_reward = 1
@@ -55,19 +54,19 @@ class Environment:
 	def test(self, agent):
 		state = self.env.reset()
 		total_reward = 0
-		state = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
-		state = cv2.resize(state, (self.HEIGHT, self.WIDTH))
+
+		state = preprocess(state, self.HEIGHT, self.WIDTH)
 		current_frame_buffer = [state, state, state, state]
 		next_frame_buffer = [state, state, state, state]
 		while True:
 			action, self.q_val = agent.predict_single(np.array(current_frame_buffer).reshape(1, self.HEIGHT, self.WIDTH, self.N_FRAMES), True)
 			next_state, reward, done, _ = self.env.step(action)
 
+			self.env.render()
 			if done: # terminal state
 				next_state = None
 			else:
-				next_state = cv2.cvtColor(next_state, cv2.COLOR_BGR2GRAY)
-				next_state = cv2.resize(next_state, (self.HEIGHT, self.WIDTH))
+				next_state = preprocess(next_state, self.HEIGHT, self.WIDTH)
 
 			next_frame_buffer.pop()
 			next_frame_buffer.append(next_state)
