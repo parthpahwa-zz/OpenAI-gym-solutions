5573b7cbc50faba85a070009f6fb56800fedd28e
Parth Pahwa <parth.pahwa95@gmail.com>
2018-03-26 17:21:23 +0530

    Update memory.py

--- DeepQNetwork/memory.py
+++ DeepQNetwork/memory.py
@@ -8,36 +8,36 @@ class Memory:
 		self.load = False
 		self.capacity = capacity
 		self.transition_list = []
-		self.under_sampled_rewards = []
-		self.under_sampled_zeros = []
-		self.length = [1.0, 1.0]
+		self.length = [0.0, 0.0, 0.0]
 		self.savefile_name = 'memory.npy'
-
+		self.reward_memory = [[], [], []]
 
 	def add(self, action_replay, perform_under_sampling=False):
 		if perform_under_sampling:
-			if action_replay[len(action_replay) - 1] :
-				if self.length[0]/self.length[1] <= 1.2:
-					self.under_sampled_rewards.append(action_replay)
+			if action_replay[len(action_replay) - 1] > 0:
+				if self.length[0]/self.capacity <= 0.334:
+					self.reward_memory[0].append(action_replay)
 					self.length[0] += 1
 				else:
-					self.under_sampled_rewards.append(action_replay)
-					self.under_sampled_rewards.pop()
+					self.reward_memory[0].append(action_replay)
+					self.reward_memory[0].pop()
 
 			elif action_replay[len(action_replay) - 1] == 0:
-				if self.length[1]/self.length[0] <= 1.2:
-					self.under_sampled_zeros.append(action_replay)
+				if self.length[1]/self.capacity <= 0.334:
+					self.reward_memory[1].append(action_replay)
 					self.length[1] += 1
 				else:
-					self.under_sampled_zeros.append(action_replay)
-					self.under_sampled_zeros.pop()
-			
-			if sum(self.length) > self.capacity + 2:
-				self.under_sampled_rewards.pop()
-				self.length[0] -= 1
+					self.reward_memory[1].append(action_replay)
+					self.reward_memory[1].pop()
 
-				self.under_sampled_zeros.pop()
-				self.length[1] -= 1
+			else:
+				if self.length[2]/self.capacity <= 0.334:
+					self.reward_memory[2].append(action_replay)
+					self.length[2] += 1
+				else:
+					self.reward_memory[2].append(action_replay)
+					self.reward_memory[2].pop()
+			print self.length, sum(self.length)
 			return
 
 		self.transition_list.append(action_replay)
@@ -50,23 +50,47 @@ class Memory:
 			if self.load:
 				self.load = False
 				for val in self.transition_list:
-					if val[len(val)-1]:
-						self.under_sampled_rewards.append(val)
+					if val[len(val)-1] > 0:
+						self.reward_memory[0].append(val)
 						self.length[0] += 1
-					else:
-						self.under_sampled_zeros.append(val)
+					elif val[len(val)-1] == 0:
+						self.reward_memory[1].append(val)
 						self.length[1] += 1
-			
+					else:
+						self.reward_memory[2].append(val)
+						self.length[2] += 1
+
 			self.transition_list = []
-			if n + 2 > sum(self.length):
+
+			if n > sum(self.length):
 				tmpList = []
-				tmpList.extend(self.under_sampled_rewards)
-				tmpList.extend(self.under_sampled_zeros)
+				for i in range(0, len(self.length)):
+					tmpList.extend(self.reward_memory[i])
 				random.shuffle(tmpList)
 				return np.array(tmpList)
 
-			self.transition_list.extend(random.sample(self.under_sampled_rewards, min(n/2, int(self.length[0])-1)))
-			self.transition_list.extend(random.sample(self.under_sampled_zeros, min(n/2, int(self.length[1])-1)))
+			remainder = n
+			counter = 0
+			flag = 1
+			while remainder > 0:
+				for i in range(0, len(self.length)):
+					if self.length[i] < int(n/len(self.length)):
+						if flag:
+							self.transition_list.extend(random.sample(self.reward_memory[i], int(self.length[i])))
+							remainder -= int(self.length[i])
+					else:
+						if remainder >= int(n/len(self.length)):
+							self.transition_list.extend(random.sample(self.reward_memory[i], int(n/len(self.length))))
+							remainder -= int(n/len(self.length))
+							counter += 1
+						elif remainder > 0:
+							if int(remainder/counter) == 0:
+								self.transition_list.extend(random.sample(self.reward_memory[i], 1))
+								remainder -= 1
+							else:
+								self.transition_list.extend(random.sample(self.reward_memory[i], int(remainder/counter)))
+								remainder -= int(remainder/counter)
+				flag = 0
 
 			random.shuffle(self.transition_list)
 			return np.array(self.transition_list)
